f651c1215fd468599867938b7e8bd1b289f64323
JSONG-205 <js_205@126.com>
2026-09-02 22:32:16 +0800

    feat: 完成变量、输入输出、类型转换三个练习

---
+++ bmi.py
@@ -0,0 +1,4 @@
+height=float(input ("请输入身高："))
+weight=float(input("请输入体重："))
+BMI=weight/(height**2)
+print ( f"BMI为{BMI:.1f}")
---
+++ calc.py
@@ -0,0 +1,4 @@
+a=float(input("请输入第一个数字:"))
+b=float(input("请输入第二个数字:"))
+
+print (f"和为{a+b},差为{a-b},积为{a*b},商为{a/b:.2f}")
