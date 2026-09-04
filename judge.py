a0e161ad2b85dca9059f211a4da008551190c3af
JSONG-205 <js_205@126.com>
2026-09-04 18:38:28 +0800

    feat:完成条件判断三个练习（BMI分级、数字判断、成绩评级)

---
+++ judge.py
@@ -0,0 +1,15 @@
+num = int(input("请输入一个整数："))
+
+# 第一组：判断正负零（三选一）
+if num > 0:
+    print("这是正数")
+elif num < 0:
+    print("这是负数")
+else:
+    print("这是零")
+
+# 第二组：判断奇偶（二选一）
+if num % 2 == 0:
+    print("这是偶数")
+else:
+    print("这是奇数")
