31988ee432e9db3f9c1ed9c0f30c7bd690f8a636
JSONG-205 <js_205@126.com>
2026-09-04 18:43:29 +0800

    feat: 给bmi.py加上体型分级

--- bmi.py
+++ bmi.py
@@ -1,4 +1,16 @@
-height=float(input ("请输入身高："))
-weight=float(input("请输入体重："))
-BMI=weight/(height**2)
-print ( f"BMI为{BMI:.1f}")
+height = float(input("请输入身高（米）："))
+weight = float(input("请输入体重（公斤）："))
+BMI = weight / (height ** 2)
+
+print(f"BMI为{BMI:.1f}")
+
+if BMI < 18.5:
+    level = "偏瘦"
+elif BMI < 24:
+    level = "正常"
+elif BMI < 28:
+    level = "偏胖"
+else:
+    level = "肥胖"
+
+print(f"你的BMI为{BMI:.1f}，你属于{level}")
