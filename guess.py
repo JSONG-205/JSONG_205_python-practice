49c2884c9c5b73c5a1cdce0e7d6598cd6f86cdd1
JSONG-205 <js_205@126.com>
2026-09-05 19:19:15 +0800

    fix: 修复第7次猜中被误判的问题

--- guess.py
+++ guess.py
@@ -13,12 +13,12 @@ else:
 while num != secret:
     num = int(input("继续猜："))
     count += 1
-    if count >= 7:
-        print(f"小妹妹你太拉了！答案是{secret}")
-        break
     if num < secret:
         print("太小了")
     elif num > secret:
         print("太大了")
     else:
         print("小妹妹你挺牛逼克拉斯啊，这都猜对了")
+    if count >= 7 and num != secret:
+        print(f"小妹妹你太拉了！答案是{secret}")
+        break
