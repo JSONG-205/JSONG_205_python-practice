10b45ad4a0df1dab2bca06eb0949233261680717
JSONG <js_205@126.com>
2026-09-05 19:00:19 +0800

    Translate guess.py to Chinese

--- guess.py
+++ guess.py
@@ -1,24 +1,23 @@
 #猜随机数
-import random
+导入 random
 secret = random.randint(1, 100)
-flag = 1
 num = int(input("来猜一个数："))
 count = 1
-if num < secret:
-    print("太小了")
-elif num > secret:
-    print("太大了")
+如果 数字 < 密码：
+    打印("太小了")
+否则如果 数字 > 密码：
+    打印("太大了")
 else:
-    print("牛逼，一发入魂")
-while num != secret:
+    打印("牛逼，一发入魂")
+当 数字 != 密码：
     num = int(input("继续猜："))
-    count += 1
-    if count >= 7:
+    计数 += 1
+    如果计数 >= 7:
         print(f"小妹妹你太拉了！答案是{secret}")
-        break
-    if num < secret:
-        print("太小了")
-    elif num > secret:
-        print("太大了")
+        中断
+    如果 数字 < 密码：
+        打印("太小了")
+    否则如果 数字 > 密码：
+        打印("太大了")
     else:
-        print("小妹妹你挺牛逼克拉斯啊，这都猜对了")
+        打印("小妹妹你挺牛逼克拉斯啊，这都猜对了")
